d38e1aa58d69505325d3cf6cb7b0a3ec0069a467
Kaan Uçarcı <info@kaanucarci.com>
2024-07-30 11:29:39 +0300

    I implemented filtering, sorting and pagination with .Net Core

--- api/Controllesrs/StockController.cs
+++ api/Controllesrs/StockController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
 using api.Dtos.Stock;
+using api.Helpers;
 using api.Interfaces;
 using api.Mappers;
 using api.Models;
@@ -26,12 +27,12 @@ namespace api.Controllesrs
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> GetAll([FromQuery] QueryObject query)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState); 
 
-            var stocks  = await _stockRepo.GetAllAsync();
+            var stocks  = await _stockRepo.GetAllAsync(query);
 
             var stockDto = stocks.Select(s => s.ToStockDto());
 
@@ -103,5 +104,7 @@ namespace api.Controllesrs
             
             return Ok(stockModel.ToStockDto());
         }        
+
+
     }
 }  
